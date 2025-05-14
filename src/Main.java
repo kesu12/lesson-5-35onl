22e16e8f6113da242b52c6b7ebeea527ded4dce9
Alexey Karpovich <drflemus@gmail.com>
2025-05-14 18:15:03 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -20,22 +22,37 @@ public class Main {
 
         //---------------------------------------------------------------------------
 
-        int[][] triangle = new int[4][5];
+//        int[][] triangle = new int[4][5];
+//
+//        for (int i = 0; i < triangle.length; i++) {
+//
+//            int ones = (i == triangle.length - 1) ? triangle[i].length : i + 1;
+//            for (int j = 0; j < triangle[i].length; j++) {
+//                triangle[i][j] = (j < ones) ? 1 : 0;
+//            }
+//        }
+//
+//
+//        for (int[] row : triangle) {
+//            System.out.println(java.util.Arrays.toString(row));
+//        }
 
-        for (int i = 0; i < triangle.length; i++) {
-            // Определяем количество единиц в текущей строке
-            int ones = (i == triangle.length - 1) ? triangle[i].length : i + 1;
-            for (int j = 0; j < triangle[i].length; j++) {
-                triangle[i][j] = (j < ones) ? 1 : 0;
-            }
+        int rows = 6;
+
+
+        int[][] triangle = new int[rows][];
+        for (int i = 0; i < rows; i++) {
+            triangle[i] = new int[i + 1]; // Длина строки = номер_строки + 1
+            Arrays.fill(triangle[i], 7);
         }
 
-        // Вывод массива
+
         for (int[] row : triangle) {
-            System.out.println(java.util.Arrays.toString(row));
+            for (int num : row) {
+                System.out.print(num);
+            }
+            System.out.println();
         }
-
-
 
 
 
