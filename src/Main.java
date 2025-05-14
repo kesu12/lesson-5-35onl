e7f7fd3efe5b879962b3b24df5c58aeedf441ccf
Alexey Karpovich <drflemus@gmail.com>
2025-05-14 18:13:00 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,42 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[][] array = new int[10][10];
-        int count = 1;
+//        int[][] array = new int[10][10];
+//        int count = 1;
+//
+//
+//        for (int i = 0; i < array.length ; i++) {
+//            for (int g = 0; g < array[i].length ; g++) {
+//                array[i][g] = count++;
+//            }
+//        }
+//
+//        for (int[] ints : array) {
+//            for (int anInt : ints) {
+//                System.out.print(anInt + " ");
+//            }
+//        }
 
+        //---------------------------------------------------------------------------
 
-        for (int i = 0; i < array.length ; i++) {
-            for (int g = 0; g < array[i].length ; g++) {
-                array[i][g] = count++;
+        int[][] triangle = new int[4][5];
+
+        for (int i = 0; i < triangle.length; i++) {
+            // Определяем количество единиц в текущей строке
+            int ones = (i == triangle.length - 1) ? triangle[i].length : i + 1;
+            for (int j = 0; j < triangle[i].length; j++) {
+                triangle[i][j] = (j < ones) ? 1 : 0;
             }
         }
 
-        for (int[] ints : array) {
-            for (int anInt : ints) {
-                System.out.print(anInt + " ");
-            }
+        // Вывод массива
+        for (int[] row : triangle) {
+            System.out.println(java.util.Arrays.toString(row));
         }
+
+
+
+
+
     }
 }
